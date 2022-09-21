0630ba4e63347f9329023d560917fbce470b9d4d
Stephanie Puertas <80504918+stephaniepuertas@users.noreply.github.com>
2022-09-21 14:51:11 -0700

    Add files via upload

---
+++ liking_likes/main.js
@@ -0,0 +1,44 @@
+// function 1
+var count =1;
+var countElement= document.querySelector("#count");
+
+function add1(){
+    count++;
+    countElement.innerText = count + " likes";
+    console.log(count);
+}
+// func for box 2
+var count2 =1;
+var countElement2= document.querySelector("#count2");
+
+function add2(){
+    count2++;
+    countElement2.innerText = count2 + " likes";
+    console.log(count2);
+}
+// func 3
+
+var count3 =1;
+var countElement3= document.querySelector("#count3");
+
+function add3(){
+    count3++;
+    countElement3.innerText = count3 + " likes";
+    console.log(count3);
+}
+// func 4
+var count4 =1;
+var countElement4= document.querySelector("#count4");
+
+function add4(){
+    count4++;
+    countElement4.innerText = count4 + " likes";
+    console.log(count4);
+}
+
+
+
+
+
+
+
